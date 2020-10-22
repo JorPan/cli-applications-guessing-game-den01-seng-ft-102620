d22ad3f3dd15d206ce8c562e13262385a6d878f5
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-10-22 13:51:36 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,9 +1,8 @@
 def run_guessing_game 
   answer = rand(6) + 1
-  #puts "Guess a number, 1-6"
-  string = gets.chomp
-  int = string.to_i
-    if int == answer
+  input = gets.chomp
+  int_input = input.to_i
+    if int_input == answer
       return "You guessed the correct number!"
     elsif string == "exit"
       puts "Goodbye!"
